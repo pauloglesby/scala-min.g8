dff0e65796921db722b9e9d54d56691eea487a03
Paul Oglesby <paul.oglesby@gmail.com>
2022-05-31 17:34:35 +0100

    Update scalafmt config.

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,14 +1,19 @@
 import sbt._
 
 object Dependencies {
-  lazy val catsVersion = "2.7.0"
 
-  lazy val catsCore: Seq[ModuleID] = Seq("org.typelevel" %% "cats-core" % catsVersion)
+  object Versions {
+    lazy val cats = "2.7.0"
+    lazy val munit = "1.0.0-M1"
+  }
 
-  lazy val baseDependencies: Seq[ModuleID] =
+  lazy val catsCore: ModuleID = "org.typelevel" %% "cats-core" % Versions.cats
+
+  lazy val baseDependencies: Seq[ModuleID] = Seq(
     catsCore
+  )
 
-  lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M1" % Test
+  lazy val munit = "org.scalameta" %% "munit" % Versions.munit % Test
 
   /* ------- MODULES --------- */
 
