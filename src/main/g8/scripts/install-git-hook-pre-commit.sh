c58f68f7f2cd8dd4a4ea3dcacce232c217221c54
Paul Oglesby <paul.oglesby@gmail.com>
2022-12-28 19:04:22 +0000

    Fix literal `$`.

--- src/main/g8/scripts/install-git-hook-pre-commit.sh
+++ src/main/g8/scripts/install-git-hook-pre-commit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PRJ_ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
+PRJ_ROOT=\$(git rev-parse --show-toplevel 2> /dev/null)
 
-cp "$PRJ_ROOT/scripts/git-hook-pre-commit.sh" "$PRJ_ROOT/.git/hooks/pre-commit"
+cp "\$PRJ_ROOT/scripts/git-hook-pre-commit.sh" "\$PRJ_ROOT/.git/hooks/pre-commit"
